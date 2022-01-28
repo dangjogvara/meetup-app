d5017363c88d1e29841874fb32375f5586eba7e1
Dan Poulsen <dangjogvara@gmail.com>
2022-01-28 19:15:17 +0000

    format

--- src/App.svelte
+++ src/App.svelte
@@ -1,112 +1,118 @@
 <script>
-    import Header from './UI/Header.svelte';
-    import MeetupGrid from './Meetups/MeetupGrid.svelte';
-    import TextInput from './UI/TextInput.svelte';
+  import Header from './UI/Header.svelte';
+  import MeetupGrid from './Meetups/MeetupGrid.svelte';
+  import TextInput from './UI/TextInput.svelte';
 
-    // Generate ID
-    import { v4 as uuidv4 } from 'uuid';
+  // Generate ID
+  import { v4 as uuidv4 } from 'uuid';
 
-    // Variables
-    let title = '';
-    let subtitle = '';
-    let address = '';
-    let imageURL = '';
-    let email = '';
-    let description = '';
+  // Variables
+  let title = '';
+  let subtitle = '';
+  let address = '';
+  let imageURL = '';
+  let email = '';
+  let description = '';
 
-    // mock data
-    let meetups = [
-        {
-            id: 'm1',
-            title: 'Coding Bootcamp',
-            subtitle: 'Learn to code',
-            description: 'In this meetup we will learn to code',
-            imageUrl:
-                    'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?bEhcYQAShJnLf0Mtu4JYq8YzICfhz2rB&size=770:433',
-            address: '24th Nerd Road, 100 Tórshavn',
-            contactEmail: 'learntocode@test.com',
-        },
-        {
-            id: 'm2',
-            title: 'Drink beer',
-            subtitle: 'Frends',
-            description: 'In this meetup we will drink beer',
-            imageUrl: 'https://picsum.photos/id/237/200/300',
-            address: '245th Nerd Road, 900 Vágur',
-            contactEmail: 'beer@test.com',
-        },
-    ];
+  // mock data
+  let meetups = [
+    {
+      id: 'm1',
+      title: 'Coding Bootcamp',
+      subtitle: 'Learn to code',
+      description: 'In this meetup we will learn to code',
+      imageUrl:
+        'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?bEhcYQAShJnLf0Mtu4JYq8YzICfhz2rB&size=770:433',
+      address: '24th Nerd Road, 100 Tórshavn',
+      contactEmail: 'learntocode@test.com',
+    },
+    {
+      id: 'm2',
+      title: 'Drink beer',
+      subtitle: 'Frends',
+      description: 'In this meetup we will drink beer',
+      imageUrl: 'https://picsum.photos/id/237/200/300',
+      address: '245th Nerd Road, 900 Vágur',
+      contactEmail: 'beer@test.com',
+    },
+  ];
 
-    // Add meetup
-    function addMeetup() {
-        const newMeetup = {
-            id: uuidv4(),
-            title,
-            subtitle,
-            description,
-            imageUrl: imageURL,
-            address,
-            contactEmail: email,
-        };
-        meetups = [newMeetup, ...meetups];
-    }
+  // Add meetup
+  function addMeetup() {
+    const newMeetup = {
+      id: uuidv4(),
+      title,
+      subtitle,
+      description,
+      imageUrl: imageURL,
+      address,
+      contactEmail: email,
+    };
+    meetups = [newMeetup, ...meetups];
+  }
 </script>
 
 <!-- header component -->
 <Header />
 
 <main>
-    <form on:submit|preventDefault={addMeetup}>
-        <TextInput
-                id="title"
-                label="Title"
-                type="text"
-                value={title}
-                on:input={(event) => title = event.target.value} />
-        <TextInput
-                id="subtitle"
-                label="Subtitle"
-                type="text"
-                value={subtitle}
-                on:input={(event) => subtitle = event.target.value} />
-        <TextInput
-                id="address"
-                label="Address"
-                type="text"
-                value={address}
-                on:input={(event) => address = event.target.value} />
-        <TextInput
-                id="imageURL"
-                label="Image URL"
-                type="text"
-                value={imageURL}
-                on:input={(event) => imageURL = event.target.value} />
-        <TextInput
-                id="email"
-                label="E-Mail"
-                value={email}
-                type="email"
-                on:input={(event) => email = event.target.value} />
-        <TextInput
-                id="description"
-                label="Description"
-                value={description}
-                controlType="textarea"
-                on:input={(event) => description = event.target.value} />
+  <form on:submit|preventDefault={addMeetup}>
+    <TextInput
+      id="title"
+      label="Title"
+      type="text"
+      value={title}
+      on:input={event => (title = event.target.value)}
+    />
+    <TextInput
+      id="subtitle"
+      label="Subtitle"
+      type="text"
+      value={subtitle}
+      on:input={event => (subtitle = event.target.value)}
+    />
+    <TextInput
+      id="address"
+      label="Address"
+      type="text"
+      value={address}
+      on:input={event => (address = event.target.value)}
+    />
+    <TextInput
+      id="imageURL"
+      label="Image URL"
+      type="text"
+      value={imageURL}
+      on:input={event => (imageURL = event.target.value)}
+    />
+    <TextInput
+      id="email"
+      label="E-Mail"
+      value={email}
+      type="email"
+      on:input={event => (email = event.target.value)}
+    />
+    <TextInput
+      id="description"
+      label="Description"
+      value={description}
+      controlType="textarea"
+      on:input={event => (description = event.target.value)}
+    />
 
-        <button type="submit">Save</button>
-    </form>
-    <MeetupGrid {meetups} />
+    <button type="submit">Save</button>
+  </form>
+  <MeetupGrid {meetups} />
 </main>
 
 <style>
-    main {
-        margin-top: 5rem;
-    }
+  main {
+    margin-top: 5rem;
+  }
 
-    form {
-        width: 30rem;
-        max-width: 90%;
-        margin: auto;
-    }
+  form {
+    width: 30rem;
+    max-width: 90%;
+    margin: auto;
+  }
 </style>
