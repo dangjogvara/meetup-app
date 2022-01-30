6c3a0761bb880218ebc5c1e66097bf47d14d7f46
Dan Poulsen <dangjogvara@gmail.com>
2022-01-30 16:22:19 +0000

    reformat

--- src/UI/TextInput.svelte
+++ src/UI/TextInput.svelte
@@ -8,43 +8,43 @@
 </script>
 
 <div class="form-control">
-    <label for={id}>{label}</label>
-    {#if controlType === 'textarea'}
-        <textarea {rows} {id} {value} on:input></textarea>
-    {:else}
-        <input {type} {id} {value} on:input/>
-    {/if}
+  <label for={id}>{label}</label>
+  {#if controlType === 'textarea'}
+    <textarea {rows} {id} {value} on:input />
+  {:else}
+    <input {type} {id} {value} on:input />
+  {/if}
 </div>
 
 <style>
-    input,
-    textarea {
-        display: block;
-        width: 100%;
-        font: inherit;
-        border: none;
-        border-bottom: 2px solid #ccc;
-        border-radius: 3px 3px 0 0;
-        background: white;
-        padding: 0.15rem 0.25rem;
-        transition: border-color 0.1s ease-out;
-    }
+  input,
+  textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    border: none;
+    border-bottom: 2px solid #ccc;
+    border-radius: 3px 3px 0 0;
+    background: white;
+    padding: 0.15rem 0.25rem;
+    transition: border-color 0.1s ease-out;
+  }
 
-    input:focus,
-    textarea:focus {
-        border-color: #e40763;
-        outline: none;
-    }
+  input:focus,
+  textarea:focus {
+    border-color: #e40763;
+    outline: none;
+  }
 
-    label {
-        display: block;
-        margin-bottom: 0.5rem;
-        width: 100%;
-    }
+  label {
+    display: block;
+    margin-bottom: 0.5rem;
+    width: 100%;
+  }
 
-    .form-control {
-        padding: 0.5rem 0;
-        width: 100%;
-        margin: 0.25rem 0;
-    }
+  .form-control {
+    padding: 0.5rem 0;
+    width: 100%;
+    margin: 0.25rem 0;
+  }
 </style>
