29be51c1303ae9010c2b46b81f9b21989e887786
Dan Poulsen <dangjogvara@gmail.com>
2022-02-05 00:11:23 +0000

    add modal and cancel functionality

--- src/Meetups/EditMeetup.svelte
+++ src/Meetups/EditMeetup.svelte
@@ -2,6 +2,7 @@
   import { createEventDispatcher } from 'svelte';
   import TextInput from '../UI/TextInput.svelte';
   import Button from '../UI/Button.svelte';
+  import Modal from '../UI/Modal.svelte';
 
   // Variables
   let title = '';
@@ -25,54 +26,54 @@
   }
 </script>
 
-<form on:submit|preventDefault={submitForm}>
-  <TextInput
-    id="title"
-    label="Title"
-    value={title}
-    on:input={event => (title = event.target.value)}
-  />
-  <TextInput
-    id="subtitle"
-    label="Subtitle"
-    value={subtitle}
-    on:input={event => (subtitle = event.target.value)}
-  />
-  <TextInput
-    id="address"
-    label="Address"
-    value={address}
-    on:input={event => (address = event.target.value)}
-  />
-  <TextInput
-    id="imageUrl"
-    label="Image URL"
-    value={imageUrl}
-    on:input={event => (imageUrl = event.target.value)}
-  />
-  <TextInput
-    id="email"
-    label="E-Mail"
-    value={email}
-    type="email"
-    on:input={event => (email = event.target.value)}
-  />
-  <TextInput
-    id="description"
-    label="Description"
-    value={description}
-    controlType="textarea"
-    on:input={event => (description = event.target.value)}
-  />
+<Modal title="Edit Meetup Data" on:cancel>
+  <form on:submit|preventDefault={submitForm}>
+    <TextInput
+      id="title"
+      label="Title"
+      value={title}
+      on:input={event => (title = event.target.value)}
+    />
+    <TextInput
+      id="subtitle"
+      label="Subtitle"
+      value={subtitle}
+      on:input={event => (subtitle = event.target.value)}
+    />
+    <TextInput
+      id="address"
+      label="Address"
+      value={address}
+      on:input={event => (address = event.target.value)}
+    />
+    <TextInput
+      id="imageUrl"
+      label="Image URL"
+      value={imageUrl}
+      on:input={event => (imageUrl = event.target.value)}
+    />
+    <TextInput
+      id="email"
+      label="E-Mail"
+      value={email}
+      type="email"
+      on:input={event => (email = event.target.value)}
+    />
+    <TextInput
+      id="description"
+      label="Description"
+      value={description}
+      controlType="textarea"
+      on:input={event => (description = event.target.value)}
+    />
 
-  <!-- Submit button -->
-  <Button type="submit">Save</Button>
-</form>
+    <!-- Submit button -->
+    <!-- <Button type="submit">Save</Button> -->
+  </form>
+</Modal>
 
 <style>
   form {
-    width: 30rem;
-    max-width: 90%;
-    margin: auto;
+    width: 100%;
   }
 </style>
