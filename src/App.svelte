83996ec802af4680b2e21568ec93f41f1e29c7b7
Dan Poulsen <dangjogvara@gmail.com>
2022-04-04 19:59:52 +0100

    lesson 117

--- src/App.svelte
+++ src/App.svelte
@@ -4,40 +4,14 @@
   import MeetupGrid from './Meetups/MeetupGrid.svelte';
   import EditMeetup from './Meetups/EditMeetup.svelte';
 
-  // Generate ID
-  import { v4 as uuidv4 } from 'uuid';
-
-  // mock data
-  let meetups = [
-    {
-      id: 'm1',
-      title: 'Coding Bootcamp',
-      subtitle: 'Learn to code',
-      description: 'In this meetup we will learn to code',
-      imageUrl:
-        'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?bEhcYQAShJnLf0Mtu4JYq8YzICfhz2rB&size=770:433',
-      address: '24th Nerd Road, 100 Tórshavn',
-      contactEmail: 'learntocode@test.com',
-      isFavorite: false,
-    },
-    {
-      id: 'm2',
-      title: 'Drink beer',
-      subtitle: 'Frends',
-      description: 'In this meetup we will drink beer',
-      imageUrl: 'https://picsum.photos/id/237/200/300',
-      address: '245th Nerd Road, 900 Vágur',
-      contactEmail: 'beer@test.com',
-      isFavorite: false,
-    },
-  ];
+  // Import store
+  import meetups from './Meetups/meetups-store';
 
   let editMode;
 
   // Add meetup
   function addMeetup(event) {
-    const newMeetup = {
-      id: uuidv4(),
+    const meetupData = {
       title: event.detail.title,
       subtitle: event.detail.title,
       description: event.detail.description,
@@ -45,18 +19,13 @@
       address: event.detail.address,
       contactEmail: event.detail.email,
     };
-    meetups = [newMeetup, ...meetups];
+    meetups.addMeetup(meetupData);
     editMode = null;
   }
 
   function toggleFavorite(event) {
     const id = event.detail;
-    const updatedMeetup = { ...meetups.find(m => m.id === id) };
-    updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
-    const meetupIndex = meetups.findIndex(m => m.id === id);
-    const updatedMeetups = [...meetups];
-    updatedMeetups[meetupIndex] = updatedMeetup;
-    meetups = updatedMeetups;
+    meetups.toggleFavorite(id);
   }
 
   function cancelEdit() {
@@ -74,7 +43,7 @@
   {#if editMode === 'add'}
     <EditMeetup on:save={addMeetup} on:cancel={cancelEdit} />
   {/if}
-  <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
+  <MeetupGrid meetups={$meetups} on:togglefavorite={toggleFavorite} />
 </main>
 
 <style>
---
+++ src/Meetups/meetups-store.js
@@ -0,0 +1,55 @@
+import { writable } from 'svelte/store';
+
+// Generate ID
+import { v4 as uuidv4 } from 'uuid';
+
+const meetups = writable([
+  {
+    id: uuidv4(),
+    title: 'Coding Bootcamp',
+    subtitle: 'Learn to code',
+    description: 'In this meetup we will learn to code',
+    imageUrl:
+      'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?bEhcYQAShJnLf0Mtu4JYq8YzICfhz2rB&size=770:433',
+    address: '24th Nerd Road, 100 Tórshavn',
+    contactEmail: 'learntocode@test.com',
+    isFavorite: false,
+  },
+  {
+    id: uuidv4(),
+    title: 'Drink beer',
+    subtitle: 'Friends',
+    description: 'In this meetup we will drink beer',
+    imageUrl: 'https://picsum.photos/id/237/200/300',
+    address: '245th Nerd Road, 900 Vágur',
+    contactEmail: 'beer@test.com',
+    isFavorite: false,
+  },
+]);
+
+const customMeetupsStore = {
+  subscribe: meetups.subscribe,
+  addMeetup: meetupData => {
+    const newMeetup = {
+      ...meetupData,
+      id: uuidv4(),
+      isFavorite: false,
+    };
+
+    meetups.update(items => {
+      return [newMeetup, ...items];
+    });
+  },
+  toggleFavorite: id => {
+    meetups.update(items => {
+      const updatedMeetup = { ...items.find(m => m.id === id) };
+      updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
+      const meetupIndex = items.findIndex(m => m.id === id);
+      const updatedMeetups = [...items];
+      updatedMeetups[meetupIndex] = updatedMeetup;
+      return updatedMeetups;
+    });
+  },
+};
+
+export default customMeetupsStore;
