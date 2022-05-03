835b686e3dd2c52b7e9f4d26f54019cb1dea9fed
Dan Poulsen <dangjogvara@gmail.com>
2022-05-03 22:01:08 +0100

    filter meetups

--- src/Meetups/MeetupGrid.svelte
+++ src/Meetups/MeetupGrid.svelte
@@ -1,12 +1,22 @@
 <script>
     import MeetupItem from './MeetupItem.svelte';
+    import MeetupFilter from './MeetupFilter.svelte';
 
     export let meetups;
-</script>
 
+    let favsOnly = false;
+    $: filteredMeetups = favsOnly ? meetups.filter(m => m.isFavorite) : meetups;
+
+    const setFilter = (event) => {
+        favsOnly = event.detail === 1;
+    };
+</script>
+<section id="meetup-controls">
+    <MeetupFilter on:select={setFilter} />
+</section>
 <section id="meetups">
     <!-- render meetup items -->
-    {#each meetups as meetup}
+    {#each filteredMeetups as meetup}
         <MeetupItem
                 id={meetup.id}
                 title={meetup.title}
@@ -23,15 +33,19 @@
 </section>
 
 <style>
-    section {
+    #meetups {
         width: 100%;
         display: grid;
         grid-template-columns: 1fr;
         grid-gap: 1rem;
     }
 
+    #meetup-controls {
+        margin: 1rem;
+    }
+
     @media (min-width: 768px) {
-        section {
+        #meetups {
             grid-template-columns: repeat(2, 1fr);
         }
     }
